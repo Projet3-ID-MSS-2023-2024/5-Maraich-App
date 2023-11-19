3773d74ff3b822577cb07cd6b83d1f04675a1265
celikesad <la218697@student.helha.be>
2023-11-19 17:33:24 +0100

    Ajout des tests du service Category
    Ajout des tests du service Category. Testant toute ces méthodes

--- Application/Backend/src/main/java/be/helha/maraichapp/services/CategoryService.java
+++ Application/Backend/src/main/java/be/helha/maraichapp/services/CategoryService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface CategoryService {
     List<Category> getAllCategories();
+    Category getCategoryById(int id);
     Category addCategory(Category category);
     Category updateCategory(Category category);
     void deleteCategoryById(int id);
--- Application/Backend/src/main/java/be/helha/maraichapp/services/CategoryServiceImpl.java
+++ Application/Backend/src/main/java/be/helha/maraichapp/services/CategoryServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
@@ -16,6 +17,12 @@ public class CategoryServiceImpl implements CategoryService{
     @Override
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category getCategoryById(int id) {
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+        return categoryOptional.orElse(null);
     }
 
     @Override
