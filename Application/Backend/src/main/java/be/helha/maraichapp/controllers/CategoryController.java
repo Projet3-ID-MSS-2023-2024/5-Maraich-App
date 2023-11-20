77973fa79b064212014be03e6400563bdc359ccb
celikesad <la218697@student.helha.be>
2023-11-20 16:21:46 +0100

    Ajout de mapping Category
    Ajout de mapping au controller de la catégorie

--- Application/Backend/src/main/java/be/helha/maraichapp/controllers/CategoryController.java
+++ Application/Backend/src/main/java/be/helha/maraichapp/controllers/CategoryController.java
@@ -8,18 +8,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/categories")
 public class CategoryController {
 
     @Autowired
     CategoryRepository categoryRepository;
 
-    @GetMapping
+    @GetMapping("/getAll")
     public List<Category> getShop(){ return categoryRepository.findAll(); }
 
-    @PostMapping
+    @PostMapping("newCategory")
     public Category addCategory(@RequestBody Category category) { return categoryRepository.save(category);}
 
-    @PutMapping
+    @PutMapping("update")
     public Category updateCategory(@RequestBody Category category){
         if (categoryRepository.existsById(category.getIdCategory()))
             return categoryRepository.save(category);
@@ -27,6 +28,6 @@ public class CategoryController {
     }
 
     @DeleteMapping
-    @RequestMapping("/{id}")
+    @RequestMapping("delete/{id}")
     public void deleteCategory(@PathVariable("id") int id) { categoryRepository.deleteById(id);}
 }
