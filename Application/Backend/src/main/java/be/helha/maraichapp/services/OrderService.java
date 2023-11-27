a16af47c9de24fb889ac70a808ea3589032a1df1
Loris-Clement <la216834@student.helha.be>
2023-11-27 09:16:46 +0100

    Create Order model and services

---
+++ Application/Backend/src/main/java/be/helha/maraichapp/services/OrderService.java
@@ -0,0 +1,14 @@
+package be.helha.maraichapp.services;
+
+import be.helha.maraichapp.models.Order;
+
+import java.util.List;
+
+public interface OrderService {
+    Order addOrder(Order order);
+    List<Order> getAllOrders();
+    Order getOrderById(int id);
+    Order updateOrder(Order order);
+    void deleteOrderById(int id);
+
+}
