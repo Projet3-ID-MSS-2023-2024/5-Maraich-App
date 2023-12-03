dcc283f2bf3259b9b9d1e99c8f1d8211e0710d50
MatteoCastin <la218187@student.helha.be>
2023-12-03 21:00:25 +0100

    Modification dans le but d'utiliser le serveur mail plutot que le serveur local de mail

--- Application/Backend/src/main/java/be/helha/maraichapp/services/EmailSender.java
+++ Application/Backend/src/main/java/be/helha/maraichapp/services/EmailSender.java
@@ -1,5 +1,6 @@
 package be.helha.maraichapp.services;
 
+import be.helha.maraichapp.models.Validation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -20,4 +21,11 @@ public class EmailSender {
         javaMailSender.send(message);
     }
 
+    public void sendActivationMail(Validation validation) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        sendEmail(validation.getUsers().getEmail(), "Your validation code",
+                String.format("Hello %s %s. Your validation code is %s; See you soon. Click here for activate your account -> http://localhost:4200/activation/%s",
+                validation.getUsers().getFirstName(), validation.getUsers().getSurname(), validation.getCode(), validation.getCode()));
+    }
 }
