8cc1813477d29e5d56826f7f1c71b3e235a11d4c
MatteoCastin <la218187@student.helha.be>
2023-12-03 04:21:41 +0100

    Consolidation de l'intégrité des données au login

--- Application/untitled/src/app/components/auth/login/login.component.ts
+++ Application/untitled/src/app/components/auth/login/login.component.ts
@@ -7,11 +7,12 @@ import {ChipsModule} from "primeng/chips";
 import {FormsModule} from "@angular/forms";
 import {AuthService} from "../../../services/auth.service";
 import {Router, RouterLink} from "@angular/router";
+import {PasswordModule} from "primeng/password";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, CheckboxModule, ButtonModule, RippleModule, ChipsModule, FormsModule, RouterLink],
+  imports: [CommonModule, CheckboxModule, ButtonModule, RippleModule, ChipsModule, FormsModule, RouterLink, PasswordModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -19,27 +20,50 @@ export class LoginComponent{
 
   userEmail!: string;
   userPassword!: string;
+  errorMessage!: string;
 
   constructor(private authService : AuthService, private route: Router) {
   }
 
-  onSubmitForm(){
-    this.authService.login(this.userEmail, this.userPassword).subscribe({
-      next:
-        (response: any) => {
-          //Enregistrez le token dans un cookie
+  onSubmitForm() {
+    this.errorMessage = "";
+    // Check that userEmail and userPassword are not null
+    if (this.userEmail && this.userPassword) {
+      // Check if the email adheres to the email regex
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(this.userEmail)) {
+        this.errorMessage = 'Échec de connexion : Veuillez fournir une adresse e-mail valide.'; // Please provide a valid email address.
+        return; // Stop execution if the email is not valid
+      }
+
+      // Check if the password adheres to the criteria
+      const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9]).{8,}$/;
+      if (!passwordRegex.test(this.userPassword)) {
+        this.errorMessage = 'Échec de connexion : Vérifiez votre mot de passe et/ou votre adresse mail.'; // The password must contain at least one uppercase letter, one digit, and have a minimum length of 8 characters.
+        return; // Stop execution if the password does not meet the criteria
+      }
+
+      // If everything is okay, call the login method
+      this.authService.login(this.userEmail, this.userPassword).subscribe({
+        next: (response: any) => {
+          // Save the token in a cookie
           document.cookie = `access_token=${response.bearer}`;
 
-          //Renvoyez vers l'affiche des maraichers A FAIRE
-           this.route.navigate(["/connexion"]);
+          // Navigate to the farmers display TO BE DONE
+          this.route.navigate(["/connexion"]);
         },
-      error:
-        (error) => {
-          //Gérez les erreurs de connexion ici
+        error: (error) => {
+          // Handle login errors here
         }
+      });
+    } else {
+      if(this.userEmail)
+        this.errorMessage = 'Échec de connexion : Veuillez fournir un mot de passe.';
+      else
+        this.errorMessage = 'Échec de connexion : Veuillez fournir une adresse email.';
     }
-    );
   }
+
 
 
 
