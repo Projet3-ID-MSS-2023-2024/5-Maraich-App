017609dc0acafb5e790ca4ad3001c36c88ebc58f
celikesad <la218697@student.helha.be>
2023-12-09 20:42:23 +0100

    Modification du controller Product
    Modification pour pouvoir upload une image en même temps qu'un produit et en lui ajoutant sa picturePath.

--- Application/Backend/src/main/java/be/helha/maraichapp/controllers/ProductController.java
+++ Application/Backend/src/main/java/be/helha/maraichapp/controllers/ProductController.java
@@ -2,9 +2,12 @@ package be.helha.maraichapp.controllers;
 
 import be.helha.maraichapp.models.Category;
 import be.helha.maraichapp.models.Product;
+import be.helha.maraichapp.services.ImageService;
 import be.helha.maraichapp.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -14,14 +17,21 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private ImageService imageService;
+
     @GetMapping("/getAll")
     public List<Product> getProduct(){ return productService.getAllProduct(); }
 
-    @GetMapping("/getAllByCategories")
+    @PostMapping("/getAllByCategories")
     public List<Product> getProductByCategories(@RequestBody Category category){ return productService.getAllProductByCategories(category); }
 
     @PostMapping("/new")
-    public Product addProduct(@RequestBody Product product) { return productService.addProduct(product);}
+    public Product addProduct(@RequestPart("product") Product product,@RequestPart("file") MultipartFile file) {
+        String fileName=imageService.saveFile(file);
+        product.setPicturePath(fileName);
+        return productService.addProduct(product);
+    }
 
     @PutMapping("/update")
     public Product updateProduct(@RequestBody Product product){
