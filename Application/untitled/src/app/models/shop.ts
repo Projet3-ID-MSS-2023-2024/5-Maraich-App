b6817dbbc32be1e82bc2855f862277b4364c9745
celikesad <la218697@student.helha.be>
2023-12-15 00:18:16 +0100

    Modif du model Product et Shop

---
+++ Application/untitled/src/app/models/shop.ts
@@ -0,0 +1,18 @@
+import {Address} from "./address";
+import {User} from "./user";
+import {Order} from "./order";
+import {Product} from "./product";
+
+export interface Shop{
+  idShop: number;
+  name: string;
+  email: string;
+  address: Address;
+  picture: string;
+  description: string;
+  shopIsOkay: boolean;
+  enable: boolean;
+  owner: User;
+  orders: Order[];
+  products: Product[];
+}
