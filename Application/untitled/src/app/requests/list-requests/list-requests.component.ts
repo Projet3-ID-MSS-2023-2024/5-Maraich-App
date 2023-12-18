60933943ddc7823aaea6b279e0be5ae59cd00c3e
Loris-Clement <la216834@student.helha.be>
2023-12-18 17:10:51 +0100

    Creating list-requests componenent

---
+++ Application/untitled/src/app/requests/list-requests/list-requests.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-requests',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-requests.component.html',
+  styleUrl: './list-requests.component.css'
+})
+export class ListRequestsComponent {
+
+}
