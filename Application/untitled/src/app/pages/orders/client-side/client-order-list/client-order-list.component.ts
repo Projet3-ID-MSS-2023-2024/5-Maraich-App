130fd46de23e75455debada7ed3eb469ff3a9e45
Loris-Clement <la216834@student.helha.be>
2024-01-02 17:02:59 +0100

    Modifying orders lists with new methods (get User orders and get Shop orders

--- Application/untitled/src/app/pages/orders/client-side/client-order-list/client-order-list.component.ts
+++ Application/untitled/src/app/pages/orders/client-side/client-order-list/client-order-list.component.ts
@@ -3,6 +3,9 @@ import {Order} from "../../../../models/order";
 import {OrderService} from "../../../../services/order.service";
 import {Router} from "@angular/router";
 import {TableModule} from "primeng/table";
+import {AuthService} from "../../../../services/auth.service";
+import {UserService} from "../../../../services/user.service";
+import {User} from "../../../../models/user";
 
 @Component({
   selector: 'app-client-order-list',
@@ -13,9 +16,21 @@ import {TableModule} from "primeng/table";
 })
 export class ClientOrderListComponent implements OnInit{
 
+  idUser!: number;
+  user!: User;
   orders: Order[] = [];
 
-  constructor(private orderService: OrderService, private router: Router) {
+  constructor(private orderService: OrderService, private router: Router, private authService: AuthService, private userService: UserService) {
+    this.idUser = this.authService.getIdUserFromCookie();
+    this.userService.getUserById(this.idUser).subscribe({
+      next: response => {
+        this.user = response;
+        console.log("Success : ", response);
+      },
+      error: error => {
+        console.error("Error : ", error);
+      }
+    })
   }
 
   ngOnInit() {
--- Application/untitled/src/app/pages/orders/vendor-side/list-order/list-order.component.ts
+++ Application/untitled/src/app/pages/orders/vendor-side/list-order/list-order.component.ts
@@ -1,8 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {ReorderableRowHandle, TableModule} from "primeng/table";
-import {Order} from "../../../models/order";
-import {OrderService} from "../../../services/order.service";
+import {Order} from "../../../../models/order";
+import {OrderService} from "../../../../services/order.service";
 import {Router} from "@angular/router";
+import {AuthService} from "../../../../services/auth.service";
+import {UserService} from "../../../../services/user.service";
+import {User} from "../../../../models/user";
 
 @Component({
   selector: 'app-list-order',
@@ -15,17 +18,36 @@ import {Router} from "@angular/router";
 })
 export class ListOrderComponent implements OnInit{
 
+  idUser!: number;
+  idShop!: number;
+  user!: User;
   orders: Order[] = [];
 
-  constructor(private orderService: OrderService, private router: Router) {
+  constructor(private orderService: OrderService, private router: Router, private authService: AuthService, private userService: UserService) {
+    this.idUser = this.authService.getIdUserFromCookie();
+    this.userService.getUserById(this.idUser).subscribe({
+      next: response => {
+        console.log("Success : ", response);
+        this.user=response;
+      },
+      error: error => {
+        console.error("Error : ", error);
+      }
+    });
+    if (this.user.shop != null) {
+      this.idShop = this.user.shop.idShop;
+    } else {
+      alert("Erreur, vous ne possédez pas de magasin. Retour sur l'acceuil");
+      this.router.navigate(['/home']);
+    }
   }
 
   ngOnInit() {
-    this.getAllOrders();
+    this.getAllOrdersFromShopSeller();
   }
 
-  getAllOrders() {
-    this.orderService.getOrders().subscribe({
+  getAllOrdersFromShopSeller() {
+    this.orderService.getOrdersByShopSellerId(this.idShop).subscribe({
       next: response => {
         this.orders = response;
         console.log("Success : ", response);
